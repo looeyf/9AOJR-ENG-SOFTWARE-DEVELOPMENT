80a1cd253f3d47108dad6215de90c2dc1f5b27e9
Luis Oliveira <luis10.pf@gmail.com>
2025-12-29 22:07:42 -0300

    feat: create project routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import todoRoutes from './routes/todoRoutes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/todos', todoRoutes);
 
 export default app;
